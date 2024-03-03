ae2311f8e88f5bb3e3049731390916aa25e9970c
Karidus-423 <kapuerto23@gmail.com>
2024-03-02 21:45:36 -0500

    C begginings

--- c_learn/leetcode/two-sum1.c
+++ c_learn/leetcode/two-sum1.c
@@ -1,23 +1,29 @@
-#include <stdio.h>
-
-void main() {
-  int nums[4] = {2, 7, 11, 15};
-  int numSize;
-  int target = 9;
-  int returnSize;
-}
-
+#include <stdlib.h>
 int *twoSum(int *nums, int numsSize, int target, int *returnSize) {
-  for (int i = 0; i < sizeof(nums); i++) {
-    for (int j = 0; j < sizeof(nums); j++) {
-      int found = nums[j] + nums[i];
-      if (target == found) {
-        int scrt[2] = {i, j};
-        printf("%f", scrt);
-        return scrt;
+  int sum;
+  int *ans = malloc(2 * sizeof(int));
+  for (int i = 0; i < numsSize; i++) {
+    for (int j = 0; j < numsSize; j++) {
+      sum = nums[i] + nums[j];
+      if (sum == target && i != j) {
+        ans[0] = i;
+        ans[1] = j;
+        *returnSize = 2;
+        printf("%d", *ans);
+        return ans;
       } else {
-        return 0;
+        sum = 0;
       }
     }
   }
+  *returnSize = 0;
+  return NULL;
+}
+
+void main() {
+  int nums[4] = {2, 7, 11, 15};
+  int numSize = 4;
+  int target = 9;
+  int *returnSize;
+  twoSum(nums, numSize, target, returnSize);
 }
